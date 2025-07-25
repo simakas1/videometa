1cc4fa0ff065a7fee28abbf4a3737f4ef5e48620
simonasvaskevicius <vaskevicius.simonas@gmail.com>
2025-07-25 10:29:21 +0300

    initialize VideoMeta project

---
+++ src/main/java/lt/svaskevicius/videometa/VideometaApplication.java
@@ -0,0 +1,13 @@
+package lt.svaskevicius.videometa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideometaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideometaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/svaskevicius/videometa/VideometaApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.svaskevicius.videometa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideometaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
